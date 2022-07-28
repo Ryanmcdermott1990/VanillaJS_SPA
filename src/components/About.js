71a289b54dcfd84416cb3e4c005e6f2563585bdd
Luke <luke@blueskylabs.com.au>
2022-07-28 21:21:15 +0800

    create state proxy in progress

--- src/components/About.js
+++ src/components/About.js
@@ -5,13 +5,6 @@ import Button from "./Button";
 import {createState} from "../Helpers/state";
 
 export default function About(mountPoint, transition) {
-    let state = createState({
-        count : 0
-    }, alert)
-    const changeState = () => {
-        state.count = state.count + 1
-    }
-
     this.node = new DOMNode(mountPoint, transition, {
         title: new Component(Title, false, {
             text: 'This is the About page!'
@@ -21,12 +14,29 @@ export default function About(mountPoint, transition) {
         })
     });
 
+    let state = createState({
+            count: 0
+        },
+        () =>  {
+            const refreshElement = this.node.element.querySelector(`[data-state="count"]`);
+            refreshElement.innerHTML = contentObj[refreshElement.getAttribute('content-func')]();
+        }
+    )
+
+    function changeState() {
+        state.count = state.count + 1
+    }
+
+    const contentObj = {
+        stateCount: () => `Count: <strong>${state.count}`
+    }
+
     const renderTemplate = () => {
         const {title, button} = this.node.children;
         return new Promise(async (myResolve) => {
             this.node.setHTML(`
                     <span data-UUID=${title.target}></span>
-                    <p>Count: <strong>${state.count}</strong></p>
+                    <p data-state="count" content-func="stateCount">${contentObj["stateCount"]()}</strong></p>
                     <span data-UUID=${button.target}></span>
                 `).then(() => {
                 this.node.renderChildren();
